6b7671486444a9f1e8ce649560c232a8b6e0e851
hopamnada <hopamnada@gmail.com>
2022-05-19 15:10:01 +0200

    up

---
+++ src/store/index.js
@@ -0,0 +1,34 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { counter: 0 },
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+  },
+});
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: { isAuth: false },
+  reducers: {
+    login(state) {
+      state.isAuth = true;
+    },
+    logout(state) {
+      state.isAuth = false;
+    },
+  },
+});
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+});
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
+export default store;
